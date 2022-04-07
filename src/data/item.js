e55869d7332740b64b4e1ac379da8e9434604194
ann <cc88030123@gmail.com>
2022-04-07 09:29:11 +0800

    add product img

--- src/data/item.js
+++ src/data/item.js
@@ -1,10 +1,10 @@
 const itemList = [
-    { id: 1, name: "OREO棉花糖餅乾", price: 55, description: "好吃的OREO棉花糖餅乾", class: "cookie", img: "https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
-    { id: 2, name: "LOTUS棉花糖餅乾", price: 55, description: "好吃的LOTUS棉花糖餅乾", class: "cookie", img: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1239&q=80" },
-    { id: 3, name: "抹茶巧克力棉花糖餅乾", price: 50, description: "好吃的抹茶巧克力棉花糖餅乾", class: "cookie", img: "https://images.unsplash.com/photo-1531171519596-47c996f5fc7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
-    { id: 4, name: "蔓越莓餅乾", price: 40, description: "好吃的蔓越莓餅乾", class: "cookie", img: "https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
-    { id: 5, name: "巴斯克乳酪蛋糕", price: 300, description: "好吃的巴斯克乳酪蛋糕", class: "other", img: "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
-    { id: 6, name: "蜂蜜燕麥磅蛋糕", price: 250, description: "好吃的蜂蜜燕麥磅蛋糕", class: "other", img: "https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" }
+    { id: 1, name: "OREO棉花糖餅乾", price: 55, description: "好吃的OREO棉花糖餅乾", class: "cookie", img: require('@/assets/product/01.jpg') },
+    { id: 2, name: "LOTUS棉花糖餅乾", price: 55, description: "好吃的LOTUS棉花糖餅乾", class: "cookie", img: require('@/assets/product/02.jpg') },
+    { id: 3, name: "抹茶巧克力棉花糖餅乾", price: 50, description: "好吃的抹茶巧克力棉花糖餅乾", class: "cookie", img: require('@/assets/product/03.jpg') },
+    { id: 4, name: "蔓越莓餅乾", price: 40, description: "好吃的蔓越莓餅乾", class: "cookie", img: require('@/assets/product/04.jpg') },
+    { id: 5, name: "巴斯克乳酪蛋糕", price: 300, description: "好吃的巴斯克乳酪蛋糕", class: "other", img: require('@/assets/product/05.jpg') },
+    { id: 6, name: "蜂蜜燕麥磅蛋糕", price: 250, description: "好吃的蜂蜜燕麥磅蛋糕", class: "other", img: require('@/assets/product/06.jpg') }
 
 
 
